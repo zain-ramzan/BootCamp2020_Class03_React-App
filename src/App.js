414a42696ed1858c702fa8556f9e95c2d17800ec
ZainRamzan <64620737+ZainRamzanGorsi@users.noreply.github.com>
2020-07-26 17:22:21 +0500

    update all

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './App.css';
-
+import Zain from './class.js';
 function App() {
-  return (
-    <div className="App">
-      <h1>Assalam-u-Alaikum! </h1>
-      <p>My name is Zain Ramzan Gorsi.</p>
-    </div>
-  );
+  return(
+<div className="App">
+
+
+        <Zain myName="Zain Ramzan Gorsi"/>
+    
+<hr />
+
+
+<hr />
+
+
+<hr />
+<Zain myName="Zain Gorsi"/>
+</div>
+
+  );  
 }
 
 export default App;
---
+++ src/class.js
@@ -0,0 +1,17 @@
+import React from 'react';
+function Zain(props) {
+    return(
+
+
+<div>
+
+<h2>My name is {props.myName}</h2>
+
+
+</div>
+    );
+    
+}
+
+
+export default Zain;
